f6931107fc64ec9d011aea2aaeba4b81172fddd2
xiplus <huangxuanyuxiplus@gmail.com>
2016-07-23 22:01:08 +0800

    diary count

--- function/diary.php
+++ function/diary.php
@@ -13,6 +13,21 @@ switch ($_POST["action"]) {
 			array("hash", getrandommd5())
 		);
 		INSERT($query);
+		$query = new query;
+		$query->table = "food";
+		$query->where = array(
+			array("fid", $_POST["fid"])
+		);
+		$row = fetchone(INSERT($query));
+		$query = new query;
+		$query->table = "food";
+		$query->value = array(
+			array("diarycnt", ($row["diarycnt"]+1))
+		);
+		$query->where = array(
+			array("fid", $_POST["fid"])
+		);
+		UPDATE($query);
 		header("Location: ../".$_POST["return"]."/?show=".$_POST["meal"]);
 		break;
 	
