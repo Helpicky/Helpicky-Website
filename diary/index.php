83b8842c60e2bd2f6d9909dcd9e0cc61ca8fbcfb
xiplus <huangxuanyuxiplus@gmail.com>
2016-07-29 19:50:47 +0800

    diary change date & count calories

--- diary/index.php
+++ diary/index.php
@@ -45,7 +45,21 @@ require("../res/template/header.php");
 ?>
 <div class="row">
 	<div class="col-xs-12 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10">
-		<h2>日記</h2>
+		<?php
+		$week = array("日", "一", "二", "三", "四", "五", "六");
+		?>
+		<h2>日記 <?php echo $date; ?> 星期<?php echo $week[date("w", strtotime($date))]; ?></h2>
+		<ul class="pager">
+			<li><a href="?date=<?php echo date("Y-m-d", strtotime($date)-86400); ?>&show=<?php echo $show; ?>">←  前一天</a></li>
+			<?php
+			if ($date < date("Y-m-d")) {
+			?>
+			<li><a href="?date=<?php echo date("Y-m-d", strtotime($date)+86400); ?>&show=<?php echo $show; ?>">後一天  →</a></li>
+			<li><a href="?date=<?php echo date("Y-m-d"); ?>&show=<?php echo $show; ?>">今天  →→</a></li>
+			<?php
+			}
+			?>
+		</ul>
 		<script type="text/javascript">
 			function change_stats(id) {
 				if (id <= 0 || id > 4) return ;
@@ -59,16 +73,17 @@ require("../res/template/header.php");
 			}
 		</script>
 		<?php
+		$sum["calories"] = 0;
 		for ($meal=1; $meal <= 4; $meal++) { 
 		?>
 		<div class="row">
-			<div class="col-xs-3 col-md-1"><img src="../res/image/diary/meal<?php echo $meal; ?>.jpg" width="50px" onclick="change_stats(<?php echo $meal; ?>)"></div>
+			<div class="col-xs-3 col-md-1"><img src="../res/image/diary/meal<?php echo $meal; ?>.png" width="50px" onclick="change_stats(<?php echo $meal; ?>)"></div>
 			<div class="col-xs-9 col-md-11">
 				<div id="meal<?php echo $meal; ?>tool" style="display: none; padding-top: 0px; padding-bottom: 0px;" class="jumbotron">
 					<button type="button" class="btn btn-info" style="color: #000; background-color: rgba(0, 0, 0, 0); border-color: rgba(0, 0, 0, 0);" onclick="alert('此功能尚未完成唷~')">
 						<span class="glyphicon glyphicon-barcode"></span>
 					</button>
-					<a href="../search/?date=<?php echo date("Y-m-d"); ?>&meal=<?php echo $meal; ?>" class="btn" role="button" style="color: #000; background-color: rgba(0, 0, 0, 0); border-color: rgba(0, 0, 0, 0);">
+					<a href="../search/?date=<?php echo $date; ?>&meal=<?php echo $meal; ?>" class="btn" role="button" style="color: #000; background-color: rgba(0, 0, 0, 0); border-color: rgba(0, 0, 0, 0);">
 						<span class="glyphicon glyphicon-search"></span>
 					</a>
 				</div>
@@ -94,8 +109,9 @@ require("../res/template/header.php");
 							array("fid", $temp["fid"])
 						);
 						$row2 = fetchone(SELECT($query2));
+						$sum["calories"] += $row2["calories"];
 						?>
-						<a href="../info/?fid=<?php echo $row2["fid"]; ?>"><?php echo $row2["name"]; ?></a><br>
+						<a href="../info/?fid=<?php echo $row2["fid"]; ?>"><?php echo $row2["name"]; ?></a>
 						<?php
 					}
 				} else {
@@ -108,11 +124,16 @@ require("../res/template/header.php");
 		<?php
 		}
 		?>
+		今日總共攝取<?php echo $sum["calories"]; ?>大卡
 <?php
 	include("../res/template/footer.php");
 ?>
 <script type="text/javascript">
-	change_stats(<?php echo $show; ?>);
+	<?php
+	foreach (explode(",", $show) as $temp) {
+		echo "change_stats(".$temp.");\n";
+	}
+	?>
 </script>
 </body>
 </html>
