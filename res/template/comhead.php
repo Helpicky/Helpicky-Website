2b74ae58d27f63b91b8538fde5d7fd028f10fb3e
xiplus <huangxuanyuxiplus@gmail.com>
2016-07-29 19:47:04 +0800

    [minor edit] icon path fix

--- res/template/comhead.php
+++ res/template/comhead.php
@@ -5,7 +5,7 @@
 <script src="../res/js/jquery.min.js"></script>
 <script src="../res/js/bootstrap.min.js"></script>
 <link rel="SHORTCUT ICON" href="../res/image/icon/icon-32.ico">
-<link rel="apple-touch-icon" href="res/image/icon/icon-60.png">
-<link rel="apple-touch-icon" sizes="76x76" href="res/image/icon/icon-76.png">
-<link rel="apple-touch-icon" sizes="120x120" href="res/image/icon/icon-120.png">
-<link rel="apple-touch-icon" sizes="152x152" href="res/image/icon/icon-152.png">
+<link rel="apple-touch-icon" href="../res/image/icon/icon-60.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../res/image/icon/icon-76.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../res/image/icon/icon-120.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../res/image/icon/icon-152.png">
