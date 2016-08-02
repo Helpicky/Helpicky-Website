6e15733ca497b3187d54e029d2a421f75cf2f3bb
xiplus <huangxuanyuxiplus@gmail.com>
2016-08-02 13:56:06 +0800

    recommend interface

--- recommend/index.php
+++ recommend/index.php
@@ -17,7 +17,7 @@ showmeta();
 require("../res/template/header.php");
 ?>
 <div class="row">
-	<div class="col-xs-12 col-sm-offset-1 col-sm-10 col-md-offset-1 col-md-10">
+	<div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-2 col-lg-8">
 		<h2>飲食推薦</h2>
 		<div class="row">
 			<div class="col-xs-12">
@@ -67,60 +67,108 @@ require("../res/template/header.php");
 						if ($temp<0.30) return 60;
 						return 0;
 					}
+					function cmp($a, $b) {
+					    if ($a["score"] == $b["score"]) {
+					        return 0;
+					    }
+					    return ($a["score"] < $b["score"]) ? 1 : -1;
+					}
 					switch ($_POST["meal"]) {
 						case '1':
 							$query = new query;
 							$query->table = "food";
 							$query->where = array(
-								array("catid", "3", null, "OR"), //蒸箱
-								array("catid", "5", null, "OR"), //關東煮
-								array("catid", "6", null, "OR"), //飯糰、壽司、手卷
-								array("catid", "8", null, "OR"), //三明治/漢堡
-								array("catid", "12", null, "OR"), //麵包
-								array("catid", "11") //蛋糕甜點
+								array("catid", "3", null, "OR"),
+								array("catid", "5", null, "OR"),
+								array("catid", "6", null, "OR"),
+								array("catid", "8", null, "OR"),
+								array("catid", "12", null, "OR"),
+								array("catid", "11")
 							);
 							$mainlist = SELECT($query);
 							$query = new query;
 							$query->table = "food";
 							$query->where = array(
-								array("catid", "1", null, "OR"), 
+								// array("catid", "1", null, "OR"), 
 								array("catid", "16", null, "OR"),
 								array("catid", "17")
 							);
 							$drinklist = SELECT($query);
-							echo "現有 ".count($mainlist)." 項主餐與 ".count($drinklist)." 項飲料進行搭配<br>";
-							$grouplist=array();
-							foreach ($mainlist as $main) {
-								foreach ($drinklist as $drink) {
-									$temp=array(
-										"carbohydrates" => $main["carbohydrates"]+$drink["carbohydrates"],
-										"fats" => $main["fats"]+$drink["fats"],
-										"protein" => $main["protein"]+$drink["protein"]
-									);
-									$temp["main"]=$main["fid"];
-									$temp["drink"]=$drink["fid"];
-									$temp["score"]=
-										diffpercent($temp["carbohydrates"], $mealnutrition["carbohydrates"])+
-										diffpercent($temp["fats"], $mealnutrition["fats"])+
-										diffpercent($temp["protein"], $mealnutrition["protein"]);
-									$grouplist[]=$temp;
-								}
-							}
-							function cmp($a, $b) {
-							    if ($a["score"] == $b["score"]) {
-							        return 0;
-							    }
-							    return ($a["score"] < $b["score"]) ? 1 : -1;
-							}
-							usort($grouplist, 'cmp');
-							foreach ($grouplist as $count => $temp) {
-								if ($count>=200) break;
-								echo getfood($temp["main"])["name"]."+".getfood($temp["drink"])["name"]." 碳水化合物:".$temp["carbohydrates"]." 脂肪:".$temp["carbohydrates"]." 蛋白質:".$temp["protein"]." ".$temp["score"]."分<br>";
-							}
+							break;
+						case '2':
+						case '3':
+							$query = new query;
+							$query->table = "food";
+							$query->where = array(
+								array("catid", "5", null, "OR"),
+								array("catid", "6", null, "OR"),
+								array("catid", "7", null, "OR"),
+								array("catid", "8", null, "OR"),
+								array("catid", "12", null, "OR")
+							);
+							$mainlist = SELECT($query);
+							$query = new query;
+							$query->table = "food";
+							$query->where = array(
+								// array("catid", "1", null, "OR"), 
+								array("catid", "16", null, "OR"),
+								array("catid", "17")
+							);
+							$drinklist = SELECT($query);
 							break;
 						default:
 							break;
 					}
+					echo "現有 ".count($mainlist)." 項主餐與 ".count($drinklist)." 項飲料進行搭配<br>";
+					$grouplist=array();
+					foreach ($mainlist as $main) {
+						foreach ($drinklist as $drink) {
+								$temp=array(
+									"carbohydrates" => $main["carbohydrates"]+$drink["carbohydrates"],
+									"fats" => $main["fats"]+$drink["fats"],
+									"protein" => $main["protein"]+$drink["protein"]
+								);
+								$temp["main"]=$main["fid"];
+								$temp["drink"]=$drink["fid"];
+								$temp["score"]=
+									diffpercent($temp["carbohydrates"], $mealnutrition["carbohydrates"])+
+									diffpercent($temp["fats"], $mealnutrition["fats"])+
+									diffpercent($temp["protein"], $mealnutrition["protein"]);
+								$grouplist[]=$temp;
+						}
+					}
+					usort($grouplist, 'cmp');
+					echo count($grouplist)."項結果<br>";
+					?>
+					<ul class="list-group" id="contact-list">
+					<?php
+					foreach ($grouplist as $count => $temp) {
+						if ($count>=200) break;
+						?>
+							<li class="list-group-item">
+								<div class="row">
+									<div class="col-xs-6 col-sm-6">
+										<a href="../info/?fid=<?php echo $temp["main"]; ?>" target="_blank"><?php echo getfood($temp["main"])["name"]; ?></a><br>
+										<a href="../info/?fid=<?php echo $temp["drink"]; ?>" target="_blank"><?php echo getfood($temp["drink"])["name"]; ?></a>
+									</div>
+									<div class="col-xs-4 col-sm-4">
+										碳水化合物:<?php echo $temp["carbohydrates"]; ?><br>
+										脂肪:<?php echo $temp["carbohydrates"]; ?><br>
+										蛋白質:<?php echo $temp["protein"]; ?><br>
+										<?php echo $temp["score"]; ?>分
+									</div>
+									<div class="col-xs-2 col-sm-2">
+										<a href="../diary/add.php?date=<?php echo $date; ?>&meal=<?php echo $meal; ?>&fid=<?php echo $food["fid"]; ?>" class="btn btn-success btn-circle" role="button" onclick="alert('新增套餐功能尚未完成'); return false;">
+											<span class="glyphicon glyphicon-plus"></span>
+										</a>
+									</div>
+								</div>
+							</li>
+						<?php
+					}
+					?>
+					</ul>
+					<?php
 				}
 				?>
 			</div>
