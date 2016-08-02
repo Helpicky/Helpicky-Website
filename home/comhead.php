bc9c5e3a1831c7c24432a9ae300eb83112507c3f
xiplus <huangxuanyuxiplus@gmail.com>
2016-08-02 22:14:46 +0800

    home new interface

---
+++ home/comhead.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width = device-width, initial-scale = 1">
+<link href="../res/css/bootstrap.min.css" rel="stylesheet">
+<link href="css.css" rel="stylesheet">
+<script src="../res/js/jquery.min.js"></script>
+<script src="../res/js/bootstrap.min.js"></script>
+<link rel="SHORTCUT ICON" href="../res/image/icon/icon-32.ico">
+<link rel="apple-touch-icon" href="../res/image/icon/icon-60.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../res/image/icon/icon-76.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../res/image/icon/icon-120.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../res/image/icon/icon-152.png">
---
+++ home/footer.php
@@ -0,0 +1,3 @@
+<div id="footer" style="position: fixed; bottom: 10px; left: 10px; opacity: 1; cursor: pointer;">
+    
+</div>
