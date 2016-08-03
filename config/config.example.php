c6fba55523886bbcb00d6045ea4d6f09ece0fab9
xiplus <huangxuanyuxiplus@gmail.com>
2016-08-03 20:06:39 +0800

    config example

---
+++ config/config.example.php
@@ -0,0 +1,24 @@
+<?php
+$cfg['facebook']['app_id'] = 'facebook_app_id';
+$cfg['facebook']['app_secret'] = 'facebook_app_secret';
+
+$cfg['cookie']['name'] = 'Helpicky';
+
+$cfg['website']['name'] = 'Helpicky';
+
+$cfg['search']['show']['number'] = 10;
+$cfg['search']['show']['threshold'] = 0;
+$cfg['search']['CTR']['cooldown'] = 60*60*24;
+
+$cfg['info']['CTR']['cooldown'] = 60*60*24;
+
+$cfg['hot']['index']['diarycnt'] = 6;
+$cfg['hot']['index']['rating'] = 3;
+$cfg['hot']['index']['CTR'] = 1;
+
+$cfg['achievement']['show_days'] = 7;
+
+$cfg['recommend']['show']['number'] = 5;
+
+$cfg['system']['require_login'] = false;
+?>
