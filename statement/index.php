4e2cac70a80ae727480c148cd767a8a341f2d8aa
xiplus <huangxuanyuxiplus@gmail.com>
2016-08-05 18:24:09 +0800

    statement

---
+++ statement/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<?php
+require("../function/common.php");
+?>
+<html lang="zh-Hant-TW">
+<head>
+<?php
+require("../res/template/comhead.php");
+showmeta();
+?>
+<title><?php echo $cfg['website']['name']; ?> 聲明</title>
+</head>
+<body topmargin="0" leftmargin="0" bottommargin="0">
+<div class="wrap">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+				<div>
+					<h1>Helpicky 聲明</h1>
+					本網站Helpicky允許以下服務，透過網頁畫面讓使用者瀏覽本網站或是連結到本網站：
+					<ul>
+						<li>Helpicky (WV) (套件 ID：org.twbbs.helpicky.helpicky_webview)</li>
+						<li>Helpicky (SC) (套件 ID：org.twbbs.helpicky.helpicky_intent)</li>
+					</ul>
+				</div>
+				<div class="text-right">
+					<span class="text-right">xiplus 開發人員<br>Helpicky 團隊</span>
+				</div>
+				<hr style="border-top: 2px solid #000">
+				<div>
+					<h1>Helpicky Statement</h1>
+					We allow the following service to provide a webview of our website or link to our website:
+					<ul>
+						<li>Helpicky (WV), org.twbbs.helpicky.helpicky_webview</li>
+						<li>Helpicky (SC), org.twbbs.helpicky.helpicky_intent</li>
+					</ul>
+				</div>
+				<div class="text-right">
+					<span class="text-right">Developer xiplus<br>The Helpicky Team</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
